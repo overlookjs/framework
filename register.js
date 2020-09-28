acaae47ff0ef2280ee1a36e86fad240cf6ea52e8
overlookmotel <theoverlookmotel@gmail.com>
2020-09-28 02:03:43 +0100

    Re-export `register` from `@overlook/plugin-build`

---
+++ register.js
@@ -0,0 +1,10 @@
+/* --------------------
+ * @overlook/framework module
+ * Register CJS entry point
+ * ------------------*/
+
+'use strict';
+
+// Exports
+
+module.exports = require('./lib/register.js');
